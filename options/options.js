3843e3dc4f333184a6bce802d64ea1ba5d730912
Crowdbotics <team@crowdbotics.com>
2023-07-31 05:23:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-dream-42537",
+  url: "https://bitter-dream-42537.botics.co",
+  api: "https://bitter-dream-42537.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
